5b005ea7a0b0e56d1a4bc49c0123ed60e1da3d6f
Muhammad Bilal <muhammadbilal56311@gmail.com>
2026-04-22 07:23:19 +0500

    Day 1 portfolio project

---
+++ Day-1-Portfolio/script.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById("btn");
+const btn1 = document.getElementById("btn1");
+
+btn.addEventListener("click", function () {
+  alert("Welcome to my portfolio!")
+  console.log("User Clicked the button")
+})
+
+btn1.addEventListener("click", function () {
+alert("Meeting booked")
+  console.log("user want to contact me")
+})
